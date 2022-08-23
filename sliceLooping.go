3ff0da359a5905e8322aaa99d9f57daa0584832f
Fahmi Tajuddin <fahmitajuddin@gmail.com>
2022-08-23 20:33:05 +0700

    assignment slice and looping

---
+++ sliceLooping.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{
+		"Giva",
+		"Iqbal",
+		"Ucup",
+		"Nugraha",
+		"Bayu",
+		"Putu",
+		"Satrio",
+		"Agus",
+		"Barru",
+		"Hasan",
+	}
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+
+}
